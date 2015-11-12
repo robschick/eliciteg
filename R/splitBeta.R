cdbf5cce62a3e8d7ad108f12792095bd7bd00240
= <=>
2015-11-12 13:32:30 +0000

    sdfg

--- R/splitBeta.R
+++ R/splitBeta.R
@@ -1,2 +1,7 @@
 #' splitBeta - takes multiple Beta distributions and splits them evenly into one Dirichlet
 #'
+#' @param alpha A number corresponding to the first parameter of the Beta distribution
+#' @param beta A number corresponding to the second parameter of the Beta distribution
+#' @param n the number of Dirichlet distributions to split the Beta into
+#'
+#' @description \code{splitBeta} is used to take the single Beta distribution returned by the experts in the Right Whale movement elicitation into multiple Dirchlet distributions. The purpose is that for the sake of expediency we collapsed several transitions into on transition, so experts only had to answer a single question.
