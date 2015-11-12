f33bd79793f8aac6373cae2430a80c6e7c1be849
= <=>
2015-11-12 13:58:32 +0000

    files delete

--- R/exploreBetaSplit.R
+++
@@ -1,7 +0,0 @@
-# this is simply to work through a basic iteration of this to show Michail
-# let's say we want to split one beta into 5 Dirichlet's.
-# We get this beta from the answers to one question regarding whales moving from SEUS north to MIDA
-# in this example the person indicates that 30 whales make the transition, and their confidence is 7:
-beta1 <- rbeta(1, 30, 7)
-
-# So!
