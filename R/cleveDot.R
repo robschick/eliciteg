11e34185cd0b4321b67adb3c0fb3a35b6b8eb7ab
= <=>
2015-11-24 15:07:55 +0000

    Rearranging this slightly to account for a different from/to orientation. Effectively I've flipped this now. Whereas before I was using fake data when I started this, now I changed it to have real data. Since these are row-wise, I had to alter the order

--- R/cleveDot.R
+++ R/cleveDot.R
@@ -1,24 +1,23 @@
 #' cleveDot - Creates faceted Cleveland-style strip/dot charts of Movement Probabilities
 #'
-#' @param n A number corresponding to the number of geographic regions
 #' @param probs Posterior movement probabilities
-#' @param from A label of length 1 corresponding to the from region
-#' @param to A vector of length n corresponding to the to regions
+#' @param from A vector of length n corresponding to the to regions
+#' @param to A label of length 1 corresponding to the from region
 #' @seealso I used recipe 3.10 from The R Graphics Cookbook, by Winston Chang
 #' @examples
-#' cleveDot(n = 9, probs = c(0.033, 0.033, 0.033, 0.184, 0.033, 0.584, 0.033, 0.033, 0.033), from = 'BOF',
+#' cleveDot(probs = c(0.033, 0.033, 0.033, 0.184, 0.033, 0.584, 0.033, 0.033, 0.033), from = 'BOF',
 #' to = c('BOF', 'GOM', 'GSC', 'JL', 'MIDA', 'NE', 'NRTH', 'RB', 'SEUS'))
-cleveDot <- function(n, probs, from, to){
+cleveDot <- function(probs, from, to){
 
-  if(!all.equal(sum(probs), 1, tolerance = .01)) stop('Probabilities do not sum to 1')
+  # if(!all.equal(sum(probs), 1, tolerance = .01)) stop('Probabilities do not sum to 1')
   df <- data.frame(from = from, to = to, moveProbs = probs)
 
-  p <- ggplot(data = df, aes(x = moveProbs, y = reorder(to, moveProbs)))+
-    geom_segment(aes(yend = to), xend = 0, colour = 'grey50')+
+  p <- ggplot(data = df, aes(x = moveProbs, y = reorder(from, moveProbs)))+
+    geom_segment(aes(yend = from), xend = 0, colour = 'grey50')+
     geom_point(size = 3)+
     coord_cartesian(xlim = c(0, 1))+
-    labs(x = 'Posterior Movement Probability', y = 'Destination Region')+
-    ggtitle(label = paste('From Region is: ', from, sep = ''))+
+    labs(x = 'Posterior Movement Probability', y = 'Source Region')+
+    ggtitle(label = paste('Destination Region is: ', df$to, sep = ''))+
     theme_bw()+
     theme(panel.grid.major.x = element_blank(),
           panel.grid.minor.x = element_blank(),
