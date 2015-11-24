347012ce64be3f77d1b8f8e91cbb422129e00c26
= <=>
2015-11-24 14:26:33 +0000

    Function to make curved connections I've added in the skeleton of a function to make the curved connections from one region to another. This uses geom_curve(), which at present is only in development - it's not in the production file.

---
+++ R/makeConn.R
@@ -0,0 +1,9 @@
+#' makeConn - Creates curved connections between Habitat Regions
+#'
+#' @param n A number corresponding to the number of geographic regions
+#' @param coords A set of x,y coordinates of nrow == n
+#' @examples
+#' makeConn(3, matrix(dat = c(1, 4, 7, 1, 5, 6), nrow = 3, ncol = 2), c(.4, .5, .1))
+makeConn <- function(n, coords, probs){
+
+}
