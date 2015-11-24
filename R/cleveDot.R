f67fe77303e92c7c4c1c142dcdf411f7ca208d88
= <=>
2015-11-24 10:23:17 +0000

    added in a test for the probability

--- R/cleveDot.R
+++ R/cleveDot.R
@@ -5,15 +5,17 @@
 #' @param from A label of length 1 corresponding to the from region
 #' @param to A vector of length n corresponding to the to regions
 #' @examples
-#' cleveDot(n = 9, probs = c(0.033, 0.033, 0.033, 0.184, 0.033, 0.584, 0.033, 0.033, 0.033), from = 'MIDA',
-#' to = c('MIDA', 'SEUS', 'BOF', 'GOM', 'GSC', 'JL', 'NE', 'NRTH', 'RB'))
+#' cleveDot(n = 9, probs = c(0.033, 0.033, 0.033, 0.184, 0.033, 0.584, 0.033, 0.033, 0.033), from = 'BOF',
+#' to = c('BOF', 'GOM', 'GSC', 'JL', 'MIDA', 'NE', 'NRTH', 'RB', 'SEUS'))
 cleveDot <- function(n, probs, from, to){
+
+  if(!all.equal(sum(probs), 1, tolerance = .01)) stop('Probabilities do not sum to 1')
   df <- data.frame(from = from, to = to, moveProbs = probs)
 
   p <- ggplot(data = df, aes(x = moveProbs, y = reorder(to, moveProbs)))+
     geom_segment(aes(yend = to), xend = 0, colour = 'grey50')+
     geom_point(size = 3)+
-    xlim(0, 1)+
+    coord_cartesian(xlim = c(0, 1))+
     labs(x = 'Posterior Movement Probability', y = 'Destination Region')+
     ggtitle(label = paste('From Region is: ', from, sep = ''))+
     theme_bw()+
