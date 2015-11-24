90378c9d2023dcbe95aea1e54cfd90bca5395bd6
= <=>
2015-11-24 10:11:55 +0000

    First check in of code for a single Cleveland strip plot This uses the R Graphics Cookbook, recipe 3.10 to make a Cleveland Style strip or dot plot of the movement probabilities.

---
+++ R/cleveDot.R
@@ -0,0 +1,24 @@
+#' cleveDot - Creates faceted Cleveland-style strip/dot charts of Movement Probabilities
+#'
+#' @param n A number corresponding to the number of geographic regions
+#' @param probs Posterior movement probabilities
+#' @param from A label of length 1 corresponding to the from region
+#' @param to A vector of length n corresponding to the to regions
+#' @examples
+#' cleveDot(n = 9, probs = c(0.033, 0.033, 0.033, 0.184, 0.033, 0.584, 0.033, 0.033, 0.033), from = 'MIDA',
+#' to = c('MIDA', 'SEUS', 'BOF', 'GOM', 'GSC', 'JL', 'NE', 'NRTH', 'RB'))
+cleveDot <- function(n, probs, from, to){
+  df <- data.frame(from = from, to = to, moveProbs = probs)
+
+  p <- ggplot(data = df, aes(x = moveProbs, y = reorder(to, moveProbs)))+
+    geom_segment(aes(yend = to), xend = 0, colour = 'grey50')+
+    geom_point(size = 3)+
+    xlim(0, 1)+
+    labs(x = 'Posterior Movement Probability', y = 'Destination Region')+
+    ggtitle(label = paste('From Region is: ', from, sep = ''))+
+    theme_bw()+
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          panel.grid.major.y = element_line(colour = 'grey60', linetype = 'dashed'))
+  print(p)
+}
