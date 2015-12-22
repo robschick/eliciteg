44a0c4369819d431ed28f55bc2008b93e4922a65
= <=>
2015-12-22 15:43:08 +0000

    Just commenting all this code out, so I don't have to run it and make the figures.

--- R/makeiGraph.R
+++ R/makeiGraph.R
@@ -1,30 +1,30 @@
-library(igraph)
-par(mfrow = c(1, 3))
-# North to MIDA
-adjm <- matrix(0, 3, 3)
-adjm[1, 2] <- 1
-g <- graph.adjacency( adjm )
-V(g)$name <- c('North', 'MIDA', 'SEUS')
-g <- set.vertex.attribute(g, 'x', value = c(-68.5, -70.4, -75.5))
-g <- set.vertex.attribute(g, 'y', value = c(43, 36, 30.6))
-plot.igraph(g, vertex.size = 20, edge.width = 3, edge.color = 'black', frame = TRUE)
-
-# loop for MIDA
-adjm <- matrix(0, 3, 3)
-adjm[2, 2] <- 1
-g <- graph.adjacency( adjm )
-V(g)$name <- c('North', 'MIDA', 'SEUS')
-g <- set.vertex.attribute(g, 'x', value = c(-68.5, -70.4, -75.5))
-g <- set.vertex.attribute(g, 'y', value = c(43, 36, 30.6))
-plot.igraph(g, vertex.size = 20, edge.width = 3, edge.color = 'black', frame = TRUE)
-
-
-
-# South to MIDA
-adjm <- matrix(0, 3, 3)
-adjm[3, 2] <- 1
-g <- graph.adjacency( adjm )
-V(g)$name <- c('North', 'MIDA', 'SEUS')
-g <- set.vertex.attribute(g, 'x', value = c(-68.5, -70.4, -75.5))
-g <- set.vertex.attribute(g, 'y', value = c(43, 36, 30.6))
-plot.igraph(g, vertex.size = 20, edge.width = 3, edge.color = 'black', frame = TRUE)
+# library(igraph)
+# par(mfrow = c(1, 3))
+# # North to MIDA
+# adjm <- matrix(0, 3, 3)
+# adjm[1, 2] <- 1
+# g <- graph.adjacency( adjm )
+# V(g)$name <- c('North', 'MIDA', 'SEUS')
+# g <- set.vertex.attribute(g, 'x', value = c(-68.5, -70.4, -75.5))
+# g <- set.vertex.attribute(g, 'y', value = c(43, 36, 30.6))
+# plot.igraph(g, vertex.size = 20, edge.width = 3, edge.color = 'black', frame = TRUE)
+#
+# # loop for MIDA
+# adjm <- matrix(0, 3, 3)
+# adjm[2, 2] <- 1
+# g <- graph.adjacency( adjm )
+# V(g)$name <- c('North', 'MIDA', 'SEUS')
+# g <- set.vertex.attribute(g, 'x', value = c(-68.5, -70.4, -75.5))
+# g <- set.vertex.attribute(g, 'y', value = c(43, 36, 30.6))
+# plot.igraph(g, vertex.size = 20, edge.width = 3, edge.color = 'black', frame = TRUE)
+#
+#
+#
+# # South to MIDA
+# adjm <- matrix(0, 3, 3)
+# adjm[3, 2] <- 1
+# g <- graph.adjacency( adjm )
+# V(g)$name <- c('North', 'MIDA', 'SEUS')
+# g <- set.vertex.attribute(g, 'x', value = c(-68.5, -70.4, -75.5))
+# g <- set.vertex.attribute(g, 'y', value = c(43, 36, 30.6))
+# plot.igraph(g, vertex.size = 20, edge.width = 3, edge.color = 'black', frame = TRUE)
