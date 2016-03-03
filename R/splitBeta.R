ed3919c36a1a2a3d4278ecb9a253a9c95c9fa021
= <=>
2016-03-03 10:04:02 +0000

    Updating this to reflect the parameterisation we're using for the individual Beta distributions. The idea is that we feed, the confidence, the number of whales / 100, and the number of regions, and we get back the value that will be populated into the different newly split entries in the prior matrix

--- R/splitBeta.R
+++ R/splitBeta.R
@@ -1,7 +1,14 @@
 #' splitBeta - takes multiple Beta distributions and splits them evenly into one Dirichlet
 #'
-#' @param alpha A number corresponding to the first parameter of the Beta distribution
-#' @param beta A number corresponding to the second parameter of the Beta distribution
-#' @param n the number of Dirichlet distributions to split the Beta into
+#' @param n A number corresponding to the first parameter of the Beta distribution; this corresponds to the confidence parameter: inputConfRaw
+#' @param m A number corresponding to the second parameter of the Beta distribution; this corresponds to the # of whales / 100, e.g. moveNortherntoMIDA / 100
+#' @param nreg the number of Dirichlet distributions to split the Beta into; this corresponds to the number of regions
 #'
 #' @description \code{splitBeta} is used to take the single Beta distribution returned by the experts in the Right Whale movement elicitation into multiple Dirchlet distributions. The purpose is that for the sake of expediency we collapsed several transitions into on transition, so experts only had to answer a single question.
+splitBeta <- function(n, m, nreg) {
+
+    expBeta1a <- (n * m) / nreg
+    expBeta1b <- (n - (n * m)) / nreg
+    list(b1 = expBeta1a, b2 = expBeta1b)
+
+}
