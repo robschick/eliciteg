16d7568f67bf1494e6d91b1313da9cee5db18044
robschick <robschick@gmail.com>
2016-09-22 09:20:06 -0400

    Modify calcC() and add in transformC()
    The goal of this was to take the exponentiation out of calcC() so that I didn't get any 0 values returned. Then I do the exponentiation in the new transformC() function. Before I take the exponenent, though, I check if any of the values in cmat are below a threshold. If so, I add a fudge factor to all of the experts in that region

--- R/calcC.R
+++ R/calcC.R
@@ -1,8 +1,8 @@
-#' Calculate c values for use in the choosing of an experts' prior
+#' Calculate raw c values for use in the choosing of an experts' prior
 #'
-#' This function calculates c weights that are the first step in
-#' choosing an individual expert's prior prior to sampling
-#' from the Dirichlet distribution within the Gibbs loop
+#' This function calculates raw, i.e. logged c weights that are the first
+#' step in choosing an individual expert's prior prior to sampling
+#' from the Dirichlet distribution within the Gibbs loop.
 #'
 
 calcC <- function(data, prior){
@@ -13,9 +13,29 @@ calcC <- function(data, prior){
 
   logc <- lgamma(sum(pp)) - sum(lgamma(pp)) - (lgamma(sum(pp + data)) - sum(lgamma(pp + data)))
 
-  return(exp(logc))
+  return(logc)
 }
 
+#' Tranform c matrix and return exponentiated values
+#'
+#' This function takes in the c matrix from the \code{calcC}
+#' function, checks to make sure the values aren't below a
+#' threshold (default value -600), and returns the exponentiated
+#' values. If the values are are below -600, then it adds a scalar.
+#' This scalar preserves the relationship between the values, which
+#' represent the prior beliefs of individual experts
+transformC <- function(cmat, tval = -600){
+
+  tvec <- apply(cmat, 2, function(x) all(x < tval))
+
+  if(any(tvec)){
+
+    cfudge <- -apply(cmat[, tvec], 2, max)
+    cmat[, tvec] <- sweep(cmat[, tvec], 2, cfudge, FUN = '+')
+
+  }
+  return(exp(cmat))
+}
 
 #' Calculate k probability vector for use in choosing of an experts' prior
 #'
